30526da3cba4bd084f4e1128bea6ede1d7abbc9c
Almeida154 <davidalmeida154of@gmail.com>
2022-05-17 00:28:15 -0300

    :construction: save() function partially ok

--- server/src/controllers/ContactController.ts
+++ server/src/controllers/ContactController.ts
@@ -1,34 +1,125 @@
 import { Request, Response } from 'express';
+import { Not } from 'typeorm';
 import { AppDataSource } from '../data-source';
 
 import { Contact } from '../entities/Contact';
+import { Telephone } from '../entities/Telephone';
+import { Email } from '../entities/Email';
 
 class ContactController {
   async save(req: Request, res: Response) {
-    const userRepository = AppDataSource.getRepository(Contact);
-    const { firstName, lastName, cpf } = req.body;
+    const contactRepository = AppDataSource.getRepository(Contact);
+    const telephoneRepository = AppDataSource.getRepository(Telephone);
+    const emailRepository = AppDataSource.getRepository(Email);
+
+    const { firstName, lastName, cpf, telephones, emails } = req.body;
+
+    if (!firstName || !lastName || telephones.length < 1)
+      return res.json({ message: 'Preencha os campos obrigatórios' });
 
     if (cpf) {
-      var contactExists = await userRepository.findOne({
+      var contactExists = await contactRepository.findOne({
         where: { cpf },
       });
-      if (contactExists)
-        return res.json({ message: 'Cpf já cadastrado' });
+      if (contactExists) return res.json({ message: 'Cpf já cadastrado' });
     }
 
-    const contact = userRepository.create({
+    const contact = contactRepository.create({
       firstName,
       lastName,
       cpf,
     });
 
-    await userRepository.save(contact);
+    await contactRepository.save(contact);
+
+    // var createdTelephones: string[] = [];
+    telephones.map(async (number: string) => {
+      var newTelephone = telephoneRepository.create({
+        number,
+        contact,
+      });
+      // createdTelephones.push(
+      //   (await telephoneRepository.save(newTelephone)).number
+      // );
+    });
+
+    // var createdEmails: string[] = [];
+    if (emails.length > 0)
+      emails.map(async (address: string) => {
+        var newEmail = emailRepository.create({
+          address,
+          contact,
+        });
+        // createdEmails.push((await emailRepository.save(newEmail)).address);
+      });
 
     return res.json(contact);
   }
 
+  async update(req: Request, res: Response) {
+    const contactRepository = AppDataSource.getRepository(Contact);
+    const { id } = req.params;
+    const { firstName, lastName, cpf } = req.body;
+
+    if (!firstName || !lastName)
+      return res.json({ message: 'Preencha os campos obrigatórios' });
+
+    if (cpf) {
+      var contactExists = await contactRepository.findOne({
+        where: { cpf, id: Not(id) },
+      });
+      if (contactExists) return res.json({ message: 'Cpf já cadastrado' });
+    }
+
+    var updatedContact = {
+      ...req.body,
+      id,
+    };
+
+    updatedContact = contactRepository.create(updatedContact);
+    await contactRepository.save(updatedContact);
+
+    updatedContact = await contactRepository.findOne({
+      where: { id },
+    });
+
+    return res.json({ updatedContact });
+  }
+
   async all(req: Request, res: Response) {
-    return res.json({ message: 'ok' });
+    const contactRepository = AppDataSource.getRepository(Contact);
+    const contacts = await contactRepository.find({
+      relations: ['emails', 'telephones'],
+    });
+    return res.json(contacts);
+  }
+
+  async one(req: Request, res: Response) {
+    const contactRepository = AppDataSource.getRepository(Contact);
+
+    if (!req.params.id) return res.json({ message: 'Algo deu errado' });
+
+    var contact = await contactRepository.findOne({
+      where: { id: req.params.id },
+      relations: ['emails', 'telephones'],
+    });
+
+    return res.json(contact);
+  }
+
+  async delete(req: Request, res: Response) {
+    const contactRepository = AppDataSource.getRepository(Contact);
+
+    if (!req.params.id) return res.json({ message: 'Algo deu errado' });
+
+    var contact = await contactRepository.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!contact) return res.json({ message: 'Algo deu errado' });
+
+    await contactRepository.remove(contact);
+    return res.json({ message: 'Contato deletado' });
   }
 }
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,9 @@ import ContactController from './controllers/ContactController';
 const routes = Router();
 
 routes.post('/contact', ContactController.save);
+routes.put('/contact/update/:id', ContactController.update);
 routes.get('/contacts', ContactController.all);
+routes.get('/contact/:id', ContactController.one);
+routes.delete('/contact/delete/:id', ContactController.delete);
 
 export default routes;
